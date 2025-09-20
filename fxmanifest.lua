c438eb54f468e3c08a44d1ddd8f6d2b2a3b6afc5
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-09-20 14:28:51 -0700

    Update fxmanifest.lua
    - update fx version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 game 'rdr3'
 lua54 'yes'
-version '1.1.0'
+version '1.1.1'
 author 'BCC Scripts'
 
 client_scripts {
